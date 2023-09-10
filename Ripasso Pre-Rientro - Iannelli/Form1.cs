9a4053d31f246b1aa10360241b39f59948952e1f
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2023-09-10 17:49:14 +0200

    Aggiungere i file di progetto.

---
+++ Ripasso Pre-Rientro - Iannelli/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ripasso_Pre_Rientro___Iannelli
+{
+    public partial class Form1 : Form
+    {
+        public string file0 = @"iannelli0.csv";
+        public string file1 = @"iannelli1.csv";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
